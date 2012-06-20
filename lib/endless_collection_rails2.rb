fce98e9cb36101c216cbbf8250fa6b287bd9df8b
Adam Trilling <adamtrilling@gmail.com>
2012-06-20 15:31:42 -0400

    use proper gem loading practices

--- lib/endless_collection_rails2.rb
+++ lib/endless_collection_rails2.rb
@@ -1,6 +1,3 @@
-#raise "hi im an error in the endless collection init"
-Rails.logger.info("loading et plugin from top-level dir")
-
 require 'endless_collections/collection'
 require 'endless_collections/endless_collection_controller'
 
